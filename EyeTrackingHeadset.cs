5cb7d19a17492d78b752fc43f992a6a346944d4c
Alexander Miller <PennNeurologyVR@gmail.com>
2018-05-14 14:23:01 -0400

    Added interface EyeTrackingHeadset.cs

---
+++ EyeTrackingHeadset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyUtility
+{
+    public interface EyeTrackingHeadset
+    {
+//-------------------------------------------------------EyeTrackingHeadset METHODS:
+
+        // side must be left or right
+        Camera GetCamera( Sides side );
+
+        Ray GetGazeConvergence();
+
+        Vector3 GetLeftEyeVector();
+
+        Vector3 GetRightEyeVector();
+
+        Vector3 GetHMDPosition();
+
+        Quaternion GetHMDRotation();
+
+        bool IsEyeTrackingCalibrated();
+
+        bool IsHardwareConnected();
+
+        bool IsHardwareReady();
+
+        void TarePosition();
+
+        void TareOrientation();
+
+        // side must be left or right
+        Vector3 WorldToViewportPoint( Vector3 pos, Sides eye );
+
+    }
+}
+
+//TODO:
+    // eyes closed
+    

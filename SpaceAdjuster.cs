c7d0cbbcd60ed5d5787878b71ee5011a58d4d1d0
Alexander Miller <PennNeurologyVR@gmail.com>
2018-08-01 13:06:01 -0400

    Moved SpaceAdjuster into MyUtility namespace

--- SpaceAdjuster.cs
+++ SpaceAdjuster.cs
@@ -1,51 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MyUtility;
 
-public class SpaceAdjuster : AxisSwapper
+namespace MyUtility
 {
+    public class SpaceAdjuster : AxisSwapper
+    {
 //------------------------------------------------------------------------CONSTANTS:
 
-    public enum TargetSpaces { World, Local, Root }
+        public enum TargetSpaces { World, Local, Root }
 
 //---------------------------------------------------------------------------FIELDS:
 	
-    public TargetSpaces Space = TargetSpaces.World;
+        public TargetSpaces Space = TargetSpaces.World;
     
 //--------------------------------------------------------------------------METHODS:
 
-    public void SetSpaceAdjustedPosition( Vector3 position )
-    {
-        switch( Space )
+        public void SetSpaceAdjustedPosition( Vector3 position )
         {
-            case TargetSpaces.World:
-                transform.position = position;
-                break;
-            case TargetSpaces.Root:
-                transform.position = transform.root.position +
-                                     transform.root.rotation * position;
-                break;
-            case TargetSpaces.Local:
-                transform.localPosition = position;
-                break;
+            switch( Space )
+            {
+                case TargetSpaces.World:
+                    transform.position = position;
+                    break;
+                case TargetSpaces.Root:
+                    transform.position = transform.root.position +
+                                         transform.root.rotation * position;
+                    break;
+                case TargetSpaces.Local:
+                    transform.localPosition = position;
+                    break;
+            }
         }
-    }
 
-    public void SetSpaceAdjustedRotation( Quaternion rotation )
-    {
-        switch( Space )
+        public void SetSpaceAdjustedRotation( Quaternion rotation )
         {
-            case TargetSpaces.World:
-                SetAttitude( rotation );
-                break;
-            case TargetSpaces.Root:
-                rotation = transform.root.rotation * rotation;
-                SetAttitude( rotation );
-                break;
-            case TargetSpaces.Local:
-                SetLocalAttitude( rotation );
-                break;
+            switch( Space )
+            {
+                case TargetSpaces.World:
+                    SetAttitude( rotation );
+                    break;
+                case TargetSpaces.Root:
+                    rotation = transform.root.rotation * rotation;
+                    SetAttitude( rotation );
+                    break;
+                case TargetSpaces.Local:
+                    SetLocalAttitude( rotation );
+                    break;
+            }
         }
     }
 }
